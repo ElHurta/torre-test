9c907ff904e4ff57b36e8a43bd8ccd2a4eb49326
ElHurta <juan.david.hurtado08@gmail.com>
2023-09-09 14:43:38 -0500

    feat: Project Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import SearchBar from './components/SearchBar'
+import ResultsList from './components/ResultsList'
 
 function App() {
 
   return (
     <>
-    
+      <header>
+        <h1>Torre Technical Test</h1>
+      </header>
+        <h2>Juan Hurtado</h2>
+      <main>
+        <h1>People By Name</h1>
+        <section>
+          <SearchBar></SearchBar>
+          <ResultsList></ResultsList>
+        </section>
+      </main>
     </>
   )
 }
